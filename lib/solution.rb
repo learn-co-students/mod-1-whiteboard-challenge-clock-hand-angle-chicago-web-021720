3e0a78409600ab9770badde6c5e43c389f41a8f9
djoverbeatz29 <jroverby92@gmail.com>
2020-03-03 11:32:12 -0600

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,6 @@
 def clock_angle(time)
-    # code your solution here
+    h, m = time.split(":").map {|i| i.to_f }
+    h = h % 12
+    ang = ((h + m / 60) * 30 - m * 6)
+    [ang, 360 - ang].min
 end
