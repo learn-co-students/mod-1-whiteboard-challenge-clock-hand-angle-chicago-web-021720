ee1585212fc78c3fa505ad813f5372267dfc54dc
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-04 08:44:51 -0600

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,12 @@
+require 'pry'
 def clock_angle(time)
     # code your solution here
+    newints = time.split(':').map{|num| num.to_f}
+    #binding.pry
+    bighand = 30*newints[0] + newints[1]/2
+    littlehand = 6*newints[1]
+    ang = (bighand - littlehand).abs
+    ang2 = 360 - ang
+    ang2 < ang ? ang2 : ang
+
 end
